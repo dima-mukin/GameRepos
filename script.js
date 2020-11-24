58f6d55b94db44cf660963f4045026dd012d6c6f
Дмитрий Мукин <d.mukin@corp.p-s.kz>
2020-11-24 11:14:03 +0600

    some new commit

---
+++ script.js
@@ -0,0 +1,50 @@
+/* ************************************** BEGIN VIEW ************************************** */
+
+var view = {
+
+	showNumber: function(n) {
+		var el = document.getElementById("showResult");
+		el.innerHTML = n;
+	}
+
+};
+
+/* ************************************** END VIEW ************************************** */
+
+
+/* ************************************** BEGIN MODEL ************************************** */
+
+var model = {
+
+	number: 0,
+
+	calculate: function(x, y) {
+		this.number = x * y;
+		var result = this.number;
+
+		view.showNumber(result);
+	}
+
+};
+
+/* ************************************** END MODEL ************************************** */
+
+
+/* ************************************** BEGIN CONTROLLER ************************************** */
+
+var controller = {
+
+	handleClick: function() {
+		model.calculate(3, 10);
+	}
+
+};
+
+/* ************************************** END CONTROLLER ************************************** */
+
+
+/* ************************************** ANONYMOUS INITIALIZE FUNCTION ************************************** */
+
+(function() {
+
+}());
